3f72826de3f2c23e9974c0358b45b8f6b00daf35
Salum Juma <ngozijuma1@gmail.com>
2025-05-13 05:06:24 -0700

    message

---
+++ settings.py
@@ -0,0 +1,8 @@
+ALLOWED_HOSTS = ['your-render-app-url.onrender.com', 'localhost', '127.0.0.1']
+
+# For production
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
