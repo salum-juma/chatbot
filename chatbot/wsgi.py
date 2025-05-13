dd712a2a41bdd3508fa958f74ca2eff66848d709
Salum Juma <ngozijuma1@gmail.com>
2025-05-13 04:47:47 -0700

    manage

--- chatbot/wsgi.py
+++ chatbot/wsgi.py
@@ -1,7 +1,26 @@
-import os
-from django.core.wsgi import get_wsgi_application
+"""core URL Configuration
 
-# Update the settings module to reflect your actual project directory name
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbot.settings')
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
 
-application = get_wsgi_application()
+urlpatterns = [
+    path('', include('apps.pages.urls')),
+    path('', include('apps.dyn_dt.urls')),
+    path('', include('apps.dyn_api.urls')),
+    path('charts/', include('apps.charts.urls')),
+    path("admin/", admin.site.urls),
+    path("", include('admin_material.urls'))
+]
