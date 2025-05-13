e6dc8c185084812dea137bab959b6ed98b3e3091
Salum Juma <ngozijuma1@gmail.com>
2025-05-13 04:56:52 -0700

    manage

--- chatbot/wsgi.py
+++ chatbot/wsgi.py
@@ -17,10 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('apps.pages.urls')),
-    path('', include('apps.dyn_dt.urls')),
-    path('', include('apps.dyn_api.urls')),
-    path('charts/', include('apps.charts.urls')),
-    path("admin/", admin.site.urls),
-    path("", include('admin_material.urls'))
+    path('', include('chatbot.apps.pages.urls')),
+    # path('', include('chatbot.apps.dyn_dt.urls')),
+    # path('', include('chatbot.apps.dyn_api.urls')),
+    # path('charts/', include('chatbot.apps.charts.urls')),
+    # path("admin/", admin.site.urls),
+    # path("", include('admin_material.urls'))
 ]
