efd9a5990dbc193b45c6065972a82788476e81e4
Salum Juma <ngozijuma1@gmail.com>
2025-06-11 14:50:33 -0700

    UPDATES PAPERS

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -190,6 +190,7 @@ class ChatSession(models.Model):
     last_message_id = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    data = models.JSONField(default=dict, blank=True)
 
     def __str__(self):
         return self.phone_number
